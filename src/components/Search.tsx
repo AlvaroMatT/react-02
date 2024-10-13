94cdf357db38551e78ed99a72e0c8e76d4497b23
AlvaroMatT <elalvaromt@gmail.com>
2024-10-13 19:39:42 +0200

    create search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,15 @@
+
+const Search = ()=>{
+    return(
+        <>
+            <div>Componente búsqueda</div>
+            <div className="flex flew-row">
+                <input className="basis-1/2 border-solid border border-white-200 bg-custom-gray"></input>
+
+                <div className="basis-1/4"></div>
+                <div className="basis-1/4"></div>
+            </div>
+        </>
+    )
+}
+export default Search
